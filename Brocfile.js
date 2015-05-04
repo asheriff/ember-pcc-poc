24367eb96bd125356ceb4487f99e7628a61ebc9b
Aaron Sheriff <asheriff@vaddio.com>
2015-05-04 12:58:41 -0500

    Added color to physical bus buttons

--- Brocfile.js
+++ Brocfile.js
@@ -4,8 +4,6 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
-app.import('bower_components/foo/bar.js');
-
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
